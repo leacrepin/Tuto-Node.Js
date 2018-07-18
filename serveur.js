ae4887024e8bacfd1cf1df1f1085d285c1a7523c
leacrepin <leacrepin@hotmail.fr>
2018-07-18 11:39:25 +0200

    18/07

--- serveur.js
+++ serveur.js
@@ -12,6 +12,11 @@ app.get('/sous-sol', function(req, res) {
     res.send('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
 });
 
+app.get('/compter/:nombre', function(req, res) {
+    var noms = ['Robert', 'Jacques', 'David'];
+    res.render('page.ejs', {compteur: req.params.nombre, noms: noms});
+});
+
 app.get('/etage/:etagenum/chambre', function(req, res) {
     res.render('chambre.ejs', {etage: req.params.etagenum});
 });
